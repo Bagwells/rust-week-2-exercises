85d58e630f53dd6ec3d6610f2b881cb95a4116b2
bagwells <timmyayomidebagwells@outlook.com>
2026-06-04 11:00:32 +0100

    Err fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
+#[allow(unused_imports)]
 use std::{fs::TryLockError::Error, num::ParseIntError};
 
 use hex::{FromHexError, decode, encode};
 
 pub fn decode_hex(hex_str: &str) -> Result<Vec<u8>, String> {
     // TODO: Decode hex string into Vec<u8>, return error string on failure
-    decode(hex_str).map_err(|e| e.to_string())
+    decode(hex_str).map_err(|e: FromHexError| e.to_string())
 }
 
 pub fn to_big_endian(bytes: &[u8]) -> Vec<u8> {
